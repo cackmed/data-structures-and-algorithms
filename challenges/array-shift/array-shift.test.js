f6a559bba0ad78ce6d317d658f19af4051549e4b
Caleb Pendergraft <cackmed@gmail.com>
2019-12-04 11:40:41 -0800

    progress

--- challenges/array-shift/array-shift.test.js
+++ challenges/array-shift/array-shift.test.js
@@ -1,8 +1,12 @@
-import insertShiftArray from './array-shift.js';
-const arr = [0, 1, 2, 3, 4, 5, 6, 7];
-const newArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const {
+  insertShiftArray
+} = require('./array-shift.js');
+
 describe('array shifting testing module', () => {
   describe('array-shifting', () => {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7];
+    const newArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    console.log(newArr);
     it('properly tells if the function inserted 8 into a existing array without deleting anything', () => {
       expect(insertShiftArray(arr).toEqual(newArr));
         
