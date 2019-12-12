26cc654b2d1297f6f90194088d111755b1b31a96
Caleb Pendergraft <cackmed@gmail.com>
2019-12-11 22:53:17 -0800

    rewrote linked list

--- challenges/linked-lists/linked-list.js
+++ challenges/linked-lists/linked-list.js
@@ -1,13 +1,83 @@
 class Node {
-  constructor(value, next = null) {
+  constructor(value) {
     this.value = value;  
-    this.next = next; 
+    this.next = null; 
   }
 }
 
 class Linkedlist {
   constructor() {
     this.head = null;
-    this.size = 0;
+  }
+  insert(value) {
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+  }
+  includes(value){
+    let current = this.head;
+
+    while(current !== null) {
+      if(current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+  toString() {
+    let string = '';
+    let current = this.head;
+
+    while(current !== null){
+      if(current.next !== null){
+        string += current.value;
+      } else {
+        string += current.value;
+      }
+      current = current.next;
+    }
+    return string;
+  }
+  append(value) {
+    const node = new Node(value);
+    let current = this.head;
+    
+    while(current !== null) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+  insertBefore(value, newValue) {
+    const node = new Node(newValue);
+    
+    if(value === this.head.value) {
+      node.next = this.head;
+      return this.head = node;
+    }
+    let current = this.head;
+    while(current.next.value !== value) {
+      current = current.next;
+    }
+    node.next = current.next;
+    current.next = node;
+  }
+  insertAfter(value, newValue) {
+    const node = new Node(newValue);
+    let current = this.head;
+    while(current.value !== value) {
+      current = current.next;
+    }
+    node.next = current.next;
+    current.next = node;
+  }
+  delete(value) {
+    let current = this.head;
+    while(current.next.value !== value) {
+      current = current.next;
+    }
+    current.next = current.next.next;
   }
 }
+
+module.exports = { Node, Linkedlist };
