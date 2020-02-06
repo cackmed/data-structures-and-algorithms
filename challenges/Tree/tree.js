48e12c86773860f805f95b35c9dcd69f515119e9
Caleb Pendergraft <cackmed@gmail.com>
2020-02-05 16:44:46 -0800

    updated code challenge

--- challenges/Tree/tree.js
+++ challenges/Tree/tree.js
@@ -1,23 +1,66 @@
 
 
 export class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
   }
 }
 
 export class BinaryTree {
-  constructor() {
-    this.root = null;
+  constructor(root = null) {
+    this.root = root;
   }
+  add(value) {
+    const node = new Node(value);
+
+    let currentNode = this.root;
+    const t = true;
+    while(t) {
+
+      if(currentNode.value <= value) {
+        if(currentNode.left) {
+          currentNode = currentNode.left;
+          continue;
+        } else {
+          currentNode.left = node;
+          break;
+        }
+      } else {
+        if(currentNode.right) {
+          currentNode = currentNode.right;
+          continue;
+        } else {
+          currentNode.right = node;
+          break;
+        }
+      }
+    }
+  }
+  contains(value) {
+    if(!this.root) return false;
+
+    let currentNode = this.root;
+    while(currentNode) {
+      if(currentNode.value === value) {
+        return true;
+      } else if(value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+
+    return false;
+  }
+
   inorder(node) 
   { 
     if(node !== null) 
     { 
       this.inorder(node.left); 
-      console.log(node.data); 
+      console.log(node.value); 
       this.inorder(node.right); 
     } 
   } 
@@ -25,7 +68,7 @@ export class BinaryTree {
   { 
     if(node != null) 
     { 
-      console.log(node.data); 
+      console.log(node.value); 
       this.preorder(node.left); 
       this.preorder(node.right); 
     } 
@@ -36,7 +79,7 @@ export class BinaryTree {
     { 
       this.postorder(node.left); 
       this.postorder(node.right); 
-      console.log(node.data); 
+      console.log(node.value); 
     } 
   } 
   
